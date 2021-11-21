5342cf9504bb4fab9330e20542380561c4d15c8f
Ian F <ian.fiddeman@gmail.com>
2021-11-21 15:21:45 +0000

    form submission, useRef, function Props and state added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import Todos from './components/Todos';
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
   return (
-    <div className="App">
-      <Todos items={['task 1', 'task 2', 'task 3']}/>
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
     </div>
   );
 }
